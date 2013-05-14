40bd26363855e748e3fda2f2daad12dbce65aca1
geesimon <geesimon@hotmail.com>
2013-05-14 14:02:25 +0800

    update print

--- assignment1/print.py
+++ assignment1/print.py
@@ -8,7 +8,6 @@ import json
 
 if __name__ == '__main__':
     url = "http://search.twitter.com/search.json?q=microsoft&page=%d"
-    
     for i in range(1,11):
         response = urllib.urlopen(url%(i))
         print json.load(response)
